ad50762657f308f315dd8789c55ed81fdef0194e
Leila Abbie <l.omol@alustudent.com>
2026-03-30 23:10:57 +0300

    Deploy React app to main branch root

--- mtaathriftfront/vite.config.ts
+++ mtaathriftfront/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "./",
+  base: "./mtaathriftfront",
   plugins: [react()],
   //root: "mtaathriftfront",
   server: {
